ed979c506c18a890fd16c1db8e1e355243b73085
Tweleman <deingesicht@twele.net>
2015-04-27 11:28:23 +0400

    controller in Bearbeitung

---
+++ hawai/src/controller/MessageReceiver.java
@@ -0,0 +1,18 @@
+package controller;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.QueueingConsumer;
+
+public class MessageReceiver {
+	
+	
+	
+	public MessageReceiver() {
+		
+	}
+	
+	
+
+}
