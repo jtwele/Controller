64dfdd6636eb226e6b38b72b440e0c54aa36b944
Tweleman <deingesicht@twele.net>
2015-04-27 17:23:03 +0200

    repo neu aufgesetzt

---
+++ controller/send/Send.java
@@ -0,0 +1,20 @@
+package controller.send;
+
+public class Send {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * IN = invoiceNinja
+		 * WW = WaWision
+		 * SG = Sugar
+		 */
+		MessageSender sender = new MessageSender();
+		sender.sendMessage("IN", "Nachricht an InvoiceNinja gesendet");
+		sender.sendMessage("WW", "Nachricht an WaWision gesendet");
+		sender.sendMessage("SG", "Nachricht an Sugar gesendet");
+		
+
+	}
+
+}
