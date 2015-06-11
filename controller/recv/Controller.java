9ddb1ad9dd40aaf17de005be1dca66baf5cef985
jeremias.twele <jeremias@twele.net>
2015-06-11 18:41:11 +0200

    umbau, weil variablen ueberschrieben wurden

---
+++ controller/recv/Controller.java
@@ -0,0 +1,91 @@
+package recv;
+
+import java.io.IOException;
+
+import messages.Messagefactory;
+import send.MessageSender;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.ConsumerCancelledException;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.ShutdownSignalException;
+
+public class Controller {
+	
+	private MessageSender sender;
+	private String receivedMessage;
+
+	public Controller(){
+		this.sender = new MessageSender();
+	}
+
+	public void start() throws ShutdownSignalException, ConsumerCancelledException, IOException, InterruptedException{
+		new Receiver("controllerInvoice", this);
+		new Receiver("controllerSugar", this);
+		new Receiver("controllerWaWision", this);
+	}
+
+	public String getReceivedMessage() {
+		return this.receivedMessage;
+	}
+
+
+	public synchronized void receiveMessage(String queuename, String message, String messageID) throws IOException{
+
+			System.out.println("***************Nachricht empfangen von : "+queuename+" ************");
+			if (true/*delivery.getProperties().getCorrelationId().matches(sender.getMessageID())*/) {
+				switch (queuename) {
+				case "controllerInvoice":
+					this.barbeiteInvoiceNachricht(message, sender.getMessageID());
+					break;
+				case "controllerSugar":
+					this.barbeiteSugarNachricht(message, sender.getMessageID());
+					break;
+				case "controllerWaWision":
+					this.barbeiteWaWisionNachricht(message, sender.getMessageID());
+					break;
+				default:
+					System.out.println("etwas ist schiefgelaufen");
+					break;
+				}
+			}
+		
+
+	}
+
+
+	private void barbeiteInvoiceNachricht(String nachricht, String messageID) {
+		// TODO: hier wird vorraussichtlich keine nachricht kommen.
+		// Hoechsten ein Acknowledge
+
+	}
+
+	private void barbeiteSugarNachricht(String nachricht, String messageID) {
+		/*
+		 * hier kommt dann ein lieferant an mit menge, preis, ... 
+		 * Aus den Daten muss dann eine RG. erstellt werden
+		 */
+
+	}
+
+	private void barbeiteWaWisionNachricht(String nachricht, String messageID) throws IOException {
+		String[] msg = nachricht.split(" ");
+		if (msg[0].matches("create"))/* ein Lieferant soll angelegt werden */{
+			sender.sendToInvoice(Messagefactory.CreateInvoiceMessage(msg), messageID);
+			
+			//this.sender.sendToInvoice(Messagefactory.CreateInvoiceMessage(nachricht, messageID));
+		}else if(msg[0].matches("bestellung")){
+				sender.sendToSugar(Messagefactory.suiteCreateFindSupplierString(msg), messageID);
+		}
+	}
+
+	public static void main(String[] args) throws InterruptedException,
+			IOException {
+		Controller cr = new Controller();
+		cr.start();
+		
+	}
+
+}
---
+++ controller/recv/Receiver.java
@@ -0,0 +1,68 @@
+package recv;
+
+import java.io.IOException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.ConsumerCancelledException;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.ShutdownSignalException;
+
+import send.MessageSender;
+
+public class Receiver {
+
+	private final String queuename;
+	private final String HOST = "141.22.29.97";
+	private final String USER = "controller";
+	private final String VHOST = "/";
+	private MessageSender sender;
+
+	private ConnectionFactory factory;
+	private Connection connection;
+	private Channel channel;
+	private QueueingConsumer consumer;
+	private String receivedMessage;
+	private Controller controller;
+	
+	public Receiver(String queuename, Controller controller) throws ShutdownSignalException, ConsumerCancelledException, IOException, InterruptedException{
+		this.queuename=queuename;
+		this.controller = controller;
+		this.receive(queuename);
+	}
+
+	
+	private void receive(String queueName) throws IOException,
+			ShutdownSignalException, ConsumerCancelledException,
+			InterruptedException {
+		this.factory = new ConnectionFactory();
+		this.createConnection();
+		this.declareQueue(queueName);
+		this.receiveMessage(queueName);
+	}
+	
+	private void receiveMessage(String queueName) throws ShutdownSignalException, ConsumerCancelledException, InterruptedException, IOException {
+		System.out.println("Warte auf Nachrichten von " + this.queuename);
+		QueueingConsumer.Delivery delivery = this.consumer.nextDelivery();
+		this.controller.receiveMessage(this.queuename, new String(delivery.getBody()), delivery.getProperties().getCorrelationId());
+		
+	}
+
+	private void createConnection() throws IOException {
+		this.factory = new ConnectionFactory();
+		this.factory.setHost(this.HOST);
+		this.factory.setUsername(this.USER);
+		this.factory.setPassword(this.USER);
+		this.factory.setVirtualHost(this.VHOST);
+		this.connection = factory.newConnection();
+		this.channel = connection.createChannel();
+	}
+	
+	private void declareQueue(String queuename) throws IOException {
+		this.channel.queueDeclare(queuename, false, false, false, null);
+		consumer = new QueueingConsumer(channel);
+		this.channel.basicConsume(queuename, true, consumer);
+	}
+
+}
