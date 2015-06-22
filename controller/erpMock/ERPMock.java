bdd339b00b8b7dc5d42d6a448c4a37ef539554c3
jeremias.twele <jeremias@twele.net>
2015-06-22 14:00:48 +0200

    test

--- controller/erpMock/ERPMock.java
+++ controller/erpMock/ERPMock.java
@@ -35,7 +35,7 @@ public class ERPMock {
 	public void handleIncomingMessage(String msg) throws IOException{
 			String[] message = MsgWrapper.createInvoiceMsg(msg);
 			System.out.println("Antwort von suagr aus bestellung:" + msg);
-			this.send(message[0]+", "+message[1] + ", " +message[2] +", "+message[3]+", " +message[4]);
+			this.send(message[0]+", "+message[1] + ", " +message[2] +", "+message[3]+", " +message[4]+ ", "+message[5]);
 			//this.send("FirmaBB"+", "+"12345" + ", " +"Lenker" +", "+"24,99"+", " +"7");
 	}
 	
